67a194e50cf13f7dfca6edf4cc2dc1fe6135974f
PedroRivadeneira <pedropeguapo@gmail.com>
2026-08-10 16:54:56 -0500

    Integra una demostracion inicial de interfaces y polimorfismo

--- src/main.go
+++ src/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/PedroRivadeneira/Sistema-Gestion-Streaming/src/interfaces"
+	"github.com/PedroRivadeneira/Sistema-Gestion-Streaming/src/modelos"
+	"github.com/PedroRivadeneira/Sistema-Gestion-Streaming/src/servicios"
+)
 
 func main() {
-	fmt.Println("Sistema de Gestión de Streaming")
+	pelicula, err := modelos.NuevaPelicula("Interestelar", "Ciencia ficción", 2014, 169)
+	if err != nil {
+		fmt.Println("Error al crear la película:", err)
+		return
+	}
+
+	serie, err := modelos.NuevaSerie("Stranger Things", "Ciencia ficción", 2016, 4)
+	if err != nil {
+		fmt.Println("Error al crear la serie:", err)
+		return
+	}
+
+	// Ambas estructuras pueden tratarse mediante la misma interfaz.
+	contenidos := []interfaces.ContenidoGestionable{pelicula, serie}
+
+	for _, contenido := range contenidos {
+		fmt.Println(servicios.MostrarContenido(contenido))
+	}
 }

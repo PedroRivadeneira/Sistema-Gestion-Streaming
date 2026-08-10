455777267660a5794b69b6840633e375ed7b6548
PedroRivadeneira <pedropeguapo@gmail.com>
2026-08-10 16:53:58 -0500

    Agrega interfaz comun para contenidos

---
+++ src/interfaces/contenido.go
@@ -0,0 +1,11 @@
+package interfaces
+
+// ContenidoGestionable define el comportamiento común de los contenidos del catálogo.
+// Pelicula y Serie implementan esta interfaz de forma implícita.
+type ContenidoGestionable interface {
+	Titulo() string
+	Genero() string
+	Anio() int
+	Disponible() bool
+	Tipo() string
+}
